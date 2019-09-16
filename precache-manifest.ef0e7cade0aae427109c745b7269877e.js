7e17e83949f22e909c585cbf7ac371cc7e9c9478
Neyasbltb <neyasbltb_88@mail.ru>
2019-09-16 22:00:36 +0300

    Updates

---
+++ precache-manifest.ef0e7cade0aae427109c745b7269877e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538d9efb2c627afce1ba5ed86d7c1e19",
+    "url": "/react-pump/index.html"
+  },
+  {
+    "revision": "489cf15338258a8c08cb",
+    "url": "/react-pump/static/css/main.adaf87c9.chunk.css"
+  },
+  {
+    "revision": "145a57ec3811324eee86",
+    "url": "/react-pump/static/js/2.0404023f.chunk.js"
+  },
+  {
+    "revision": "489cf15338258a8c08cb",
+    "url": "/react-pump/static/js/main.5bd583cf.chunk.js"
+  },
+  {
+    "revision": "ec7ef9af9f8643152d31",
+    "url": "/react-pump/static/js/runtime~main.197615e8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "react-pump/precache-manifest.83862b4b8813f3e48e4ccda6f22161de.js"
+  "/react-pump/precache-manifest.ef0e7cade0aae427109c745b7269877e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("react-pump/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pump/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
