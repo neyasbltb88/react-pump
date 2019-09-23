854984e61a30240ea30ce7641035e0bbfc3b03d8
Neyasbltb <neyasbltb_88@mail.ru>
2019-09-23 19:46:19 +0300

    commit

--- deploy_auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    user: "neyasbltb",
-    password: "ker_fess",
-    host: "files.000webhost.com",
-    port: 21,
-    hostFolder: "react-pump/",
-    remoteRoot: "/public_html/",
-    baseUrl: "http://fess88.ru/"
-}
